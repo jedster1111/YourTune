a34c5bc3bea0e466ebeade6b4341ae298201cc1a
Jed <jedster1111@hotmail.co.uk>
2019-01-13 01:34:01 +0000

    Readded SIGINT handler in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -52,6 +52,6 @@ export const server = app.listen(PORT);
 
 console.log(`Server is listening on port ${PORT}`);
 
-// process.on("SIGINT", () => {
-//   process.exit();
-// });
+process.on("SIGINT", () => {
+  process.exit();
+});
